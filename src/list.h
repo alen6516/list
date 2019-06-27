6ca3271508a2ec4dca50221739bcb9efd597b2f1
alen6516 <alen6516@gmail.com>
2019-06-27 12:43:57 +0800

    update list.h

--- src/list.h
+++ src/list.h
@@ -3,30 +3,37 @@
 #ifndef LIST_H
 #define LIST_H
 
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+
 #include <stdio.h>
 #include <stdlib.h>
 
+
 // Version
 #define LIST_VERSION "0.0.1"
+
 
 // Memory management macros
 #ifndef LIST_MALLOC
 #define LIST_MALLOC malloc
 #endif
 
+
 #ifndef LIST_FREE
 #define LIST_FREE free
 #endif
+ 
 
 // list_t node struct
 typedef struct list_node{
     struct list_node *next;
     void *val;
 } list_node_t;
+
 
 // list_t struct
 typedef struct {
@@ -37,40 +44,37 @@ typedef struct {
     int (*match)(void *a, void *b);
 } list_t;
 
+
 // list_t iterator struct
 typedef struct {
     list_node_t *next;
 } list_iterator_t;
 
+
 // Node prototypes
 list_node_t *list_node_new(void *val);
 
+
 // list_t prototypes
 list_t *list_new();
-
 list_node_t *list_push(list_t *self, list_node_t *node);
-
 list_node_t *list_find(list_t *self, void *val);
-
 list_node_t *list_at(list_t *self, int index);
-
 list_node_t *list_pop(list_t *self);
-
 void list_remove(list_t *self, list_node_t *node);
-
 void list_destroy(list_t *self);
+
 
 // list iterator prototypes
 list_iterator_t *list_iterator_new(list_t *list);
-
 list_iterator_t *list_iterator_new_from_node(list_node_t *node);
-
 list_node_t *list_iterator_next(list_iterator_t *self);
-
 void list_iterator_destroy(list_iterator_t *self);
+
 
 #ifdef __cplusplus
 }
 #endif
+
 
 #endif  /* LIST_H */
